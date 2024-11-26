611def3a1aeece59ef4d1939797315cc13dffa14
tlsrms <kpipi1219@naver.com>
2024-11-27 02:43:24 +0900

    client_handler.h 구현

--- server/includes/client_handler.h
+++ server/includes/client_handler.h
@@ -1,1 +1,11 @@
+#ifndef CLIENT_HANDLER.H
+#define CLIENT_HANDLER.H
 
+#include "common.h"
+
+int is_user_registered(const char *username);
+void register_user(const char *username);
+void remove_client(int sockfd);
+void *handle_client(void *arg);
+
+#endif

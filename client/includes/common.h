03f1a44cd0a6db72ddb74199c78b299a62b91f32
tlsrms <kpipi1219@naver.com>
2024-11-27 02:42:58 +0900

    server/client common.h 구현

--- client/includes/common.h
+++ client/includes/common.h
@@ -1,1 +1,8 @@
+#ifndef COMMON_H
+#define COMMON_H
 
+#define PORT 8080
+#define BUFFER_SIZE 4096
+#define USER_FILE "user.txt"
+
+#endif
--- server/includes/common.h
+++ server/includes/common.h
@@ -1,1 +1,20 @@
+#include <pthread.h>
+#ifndef COMMON_H
+#define COMMON_H
 
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
+#define PORT 8080
+#define USER_FILE "user.txt"
+
+typedef struct {
+    int sockfd;
+    char username[50];
+    pthread_t thread;
+} Client;
+
+Client clients[MAX_CLIENTS];      
+int client_count;                 
+pthread_mutex_t clients_mutex;    
+
+#endif

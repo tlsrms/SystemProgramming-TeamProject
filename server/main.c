e5a8d2658d61664ec0e1f22a6c789f727aecfe9a
jihui0905 <151699232+jihui0905@users.noreply.github.com>
2024-12-01 14:11:28 +0900

    Update main.c

---
+++ server/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+void receive_log(int sock);
+
+int main() {
+    int client_sock;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+
+    // 소켓 생성
+    client_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_sock == -1) {
+        perror("소켓 생성 실패");
+        return 1;
+    }
+
+    // 서버 주소 설정
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(PORT);
+
+    // 서버 연결
+    if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("서버 연결 실패");
+        close(client_sock);
+        return 1;
+    }
+
+    printf("GET_CHAT_LOG 또는 GET_FILE_LOG 입력: ");
+    fgets(buffer, BUFFER_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = '\0';
+
+    send(client_sock, buffer, strlen(buffer), 0);
+    receive_log(client_sock);
+
+    close(client_sock);
+    return 0;
+}

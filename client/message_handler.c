be882b738525eecda7f82a810b1dedc222c3bf5a
jihui0905 <151699232+jihui0905@users.noreply.github.com>
2024-12-01 14:13:18 +0900

    Update message_handler.c

--- client/message_handler.c
+++ client/message_handler.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
+void receive_log(int sock) {
+    char buffer[BUFFER_SIZE];
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        recv(sock, buffer, BUFFER_SIZE - 1, 0);
+        if (strcmp(buffer, "END_OF_CHAT_LOG\n") == 0 || strcmp(buffer, "END_OF_FILE_LOG\n") == 0) break;
+        printf("%s", buffer);
+    }
+}

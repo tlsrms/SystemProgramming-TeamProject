86908f20b98815c13d5d1c3153ddad8de62f67e1
tlsrms <kpipi1219@naver.com>
2024-12-03 03:28:48 +0900

    client/common.h 수정

--- client/includes/common.h
+++ client/includes/common.h
@@ -1,6 +1,6 @@
-#include <pthread.h>
 #ifndef COMMON_H
 #define COMMON_H
+#include <pthread.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 4096
@@ -14,6 +14,12 @@ typedef struct
 	char message[BUFFER_SIZE];	 // 채팅 메시지
 	char file_data[BUFFER_SIZE]; // 파일 데이터
 } Packet;
+
+pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER; //공유 변수 접근시 사용
+pthread_mutex_t send_mutex = PTHREAD_MUTEX_INITIALIZER; //send 함수 사용시
+pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER; // shared_file.txt 사용시
+
+int keep_running = 1; //나중에 모든 무한 반복문 조건을 이 변수로 교체(/quit 호출시 0으로 변경하여 스레드 자원 해제)
 
 // 작업 큐 정의
 #define QUEUE_SIZE 100

1d9bedad33a5c9e9117e45129b8ca871d8531ae3
sunflow-er <antjd03170@gmail.com>
2024-12-03 03:02:14 +0900

    채팅 log 저장 및 broadcast 기능 수정

--- server/broadcast.c
+++ server/broadcast.c
@@ -1,8 +1,6 @@
 #include <pthread.h>
 #include "includes/common.h"
 
-pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
-
 /**
  * @brief 모든 클라이언트에게 패킷을 브로드캐스트하는 함수.
  *
@@ -19,15 +17,11 @@ pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
  *                     - 일반적으로 메시지를 보낸 클라이언트의 소켓 번호
  */
 void broadcast_packet(Packet *packet, Client clients[], int client_count, int exclude_sock) {
-    pthread_mutex_lock(&clients_mutex); // lock, client 배열에 동시 접근을 막기 위해 사용
-
 	// 자기 자신을 제외한 연결된 모든 클라이언트에게 데이터 전송
     for (int i = 0; i < client_count; i++) {
         if (clients[i].sockfd != exclude_sock) {
             send(clients[i].sockfd, packet, sizeof(Packet), 0);
         }
     }
-
-    pthread_mutex_unlock(&clients_mutex); // unlock
 }
 
--- server/chat_handler.c
+++ server/chat_handler.c
@@ -4,20 +4,18 @@
 #include "includes/chat_handler.h"
 #include "includes/broadcast.h"
 
-extern pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER;
-
 /**
  * @brief 클라이언트로부터 받은 채팅 메시지를 처리하는 함수.
  * 
  * @param packet       클라이언트로부터 수신한 데이터 패킷의 포인터.
  *                     - `username`: 메시지를 보낸 사용자 이름.
  *                     - `message`: 보낸 채팅 메시지.
- * @param sender_sock  메시지를 보낸 클라이언트의 소켓 파일 디스크립터.
+ * @param client_socket  메시지를 보낸 클라이언트의 소켓 파일 디스크립터.
  *                     - 이 소켓은 브로드캐스트 시 제외 대상이 됨.
  */
-void handle_chat_message(Packet *packet, int sender_sock) {
+void handle_chat_message(Packet *packet, int client_socket) {
     save_chat_log(packet->username, packet->message); // 채팅 로그 저장 
-    broadcast_packet(packet, clients, client_count, sender_sock); // 다른 클라이언트에게 채팅 보내기
+    broadcast_packet(packet, clients, client_count, client_socket); // 다른 클라이언트에게 채팅 보내기
 }
 
 
@@ -25,16 +23,14 @@ void handle_chat_message(Packet *packet, int sender_sock) {
  * @brief 채팅 로그를 파일에 저장하는 함수.
  * 
  * @param username 메시지를 보낸 사용자의 이름.
- *                 - 로그에 "[username]: message" 형식으로 기록.
+ *                 - 로그에 "(username), (message)" 형식으로 기록.
  * @param message  저장할 채팅 메시지 내용.
  */
 void save_chat_log(const char *username, const char *message) {
-    pthread_mutex_lock(&file_mutex); // lock, 파일 동시 접근 방지
     FILE *log = fopen(CHAT_LOG, "a"); // 추가 모드(a)로 파일 열기
     if (log) {
         fprintf(log, "%s, %s\n", username, message);
         fclose(log);
     }
-    pthread_mutex_unlock(&file_mutex); // unlock
 }
 

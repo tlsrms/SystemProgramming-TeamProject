865be7e71baf4a77bca1d6d3214d528f0c4cc5ad
jihui <sjh101512@naver.com>
2024-12-05 02:01:36 +0900

    add FILE define

--- server/client_handler.c
+++ server/client_handler.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
+#include <stdio.h>      // fopen(), fgets(), fclose(), printf() 등의 함수 사용을 위해 필요
+#include <string.h>     // strlen() 등의 함수 사용을 위해 필요
+#include <unistd.h>     // send() 함수 사용을 위해 필요
+#include <pthread.h>    // pthread_mutex_t, pthread_mutex_lock, pthread_mutex_unlock 등을 사용하기 위해 필요
 
+#define FILE_NAME_SIZE 64
+#define FILE_PATH_SIZE 128
+#define BUFFER_SIZE 4096
 #define CHAT_LOG "chat_log.txt"
 #define FILE_LOG "file_log.txt"
-#define BUFFER_SIZE 4096
 
 pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void send_chat_log(int client_sock) {
     pthread_mutex_lock(&file_mutex);
 
-    FILE *log = fopen(CHAT_LOG, "r");
+    FILE *log = fopen(CHAT_LOG, "r");  
     char line[BUFFER_SIZE];
     if (log) {
         while (fgets(line, sizeof(line), log)) {
@@ -28,7 +30,7 @@ void send_chat_log(int client_sock) {
 void send_file_log(int client_sock) {
     pthread_mutex_lock(&file_mutex);
 
-    FILE *log = fopen(FILE_LOG, "r");
+    FILE *log = fopen(FILE_LOG, "r");  
     char line[BUFFER_SIZE];
     if (log) {
         while (fgets(line, sizeof(line), log)) {
@@ -39,37 +41,4 @@ void send_file_log(int client_sock) {
     send(client_sock, "END_OF_FILE_LOG\n", 17, 0);
 
     pthread_mutex_unlock(&file_mutex);
-}
-
-void *handle_client(void *arg) {
-    int client_sock = *(int *)arg;
-    free(arg);
-    char buffer[BUFFER_SIZE];
-
-    while (1) {
-        memset(buffer, 0, BUFFER_SIZE);
-        int bytes_received = recv(client_sock, buffer, BUFFER_SIZE - 1, 0);
-        if (bytes_received <= 0) {
-            printf("클라이언트 연결 종료\n");
-            break;
-        }
-        buffer[bytes_received] = '\0';
-
-        if (strcmp(buffer, "GET_CHAT_LOG") == 0) {
-            send_chat_log(client_sock);
-        } else if (strcmp(buffer, "GET_FILE_LOG") == 0) {
-            send_file_log(client_sock);
-        } else {
-            pthread_mutex_lock(&file_mutex);
-            FILE *log = fopen(CHAT_LOG, "a");
-            if (log) {
-                fprintf(log, "%s\n", buffer);
-                fclose(log);
-            }
-            pthread_mutex_unlock(&file_mutex);
-        }
-    }
-
-    close(client_sock);
-    return NULL;
 }

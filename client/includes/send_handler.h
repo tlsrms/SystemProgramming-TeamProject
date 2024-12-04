ce7d6f47cc9848a8467692891048e7f9a91f1444
sunflow-er <antjd03170@gmail.com>
2024-12-05 01:31:28 +0900

    /commit, /rebase, /log 처리

--- client/includes/send_handler.h
+++ client/includes/send_handler.h
@@ -7,6 +7,9 @@ void *send_terminal_packet(void *arg);
 
 void command_new();
 void command_load(const char *input);
+void command_commit(const char *input);
+void command_rebase(const char *input);
+void command_log();
 void command_quit();
 void send_chat_message(const char *input);
 
